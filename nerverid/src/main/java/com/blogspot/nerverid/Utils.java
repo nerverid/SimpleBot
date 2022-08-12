dc07e73a1da3bbc4f4c299d4c83478682c247cbc
Konstantin <nero-add@mail.ru>
2022-08-12 18:58:02 +0700

    Try releaze what I do this

--- nerverid/src/main/java/com/blogspot/nerverid/Utils.java
+++ nerverid/src/main/java/com/blogspot/nerverid/Utils.java
@@ -1,5 +1,17 @@
 package com.blogspot.nerverid;
 
-public class Utils {
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.User;
 
+public class Utils {
+	
+	public static String getUserName(Message msg) {
+		return getUserName(msg.getFrom());
+	}
+	
+	public static String getUserName(User user) {
+		return (user.getUserName() != null) ? user.getUserName() :
+			String.format("%s %s", user.getLastName(), user.getFirstName());
+	}
 }
+
